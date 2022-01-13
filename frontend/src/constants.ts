8429dcae4dc48575858e35941d6005a0347f0ca1
Faris Mohamed <faris@posteo.net>
2022-01-12 23:35:02 -0500

    bad ternary statement

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,5 @@
-export const baseURL = `http://localhost:8000`
-    ? import.meta.env.MODE === 'development'
-    : `https://api.schedmeet.com/`
+export const baseURL =
+    import.meta.env.MODE === 'development'
+        ? `http://localhost:8000`
+        : `https://api.schedmeet.com/`
 export const testAuthPath = `${baseURL}/test_auth`
