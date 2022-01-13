07ac8c0ff68bf374155b8a3acab61bcfff9dd54f
Faris Mohamed <faris@posteo.net>
2022-01-12 23:40:16 -0500

    double `/` char messes up requests

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,5 +1,5 @@
 export const baseURL =
     import.meta.env.MODE === 'development'
         ? `http://localhost:8000`
-        : `https://api.schedmeet.com/`
+        : `https://api.schedmeet.com`
 export const testAuthPath = `${baseURL}/test_auth`
