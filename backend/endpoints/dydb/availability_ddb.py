44a097cf22fbabea1363ef584ef84c9545e75867
Faris Mohamed <faris@posteo.net>
2022-04-22 02:56:54 -0400

    feature parity between two api's

--- backend/endpoints/dydb/availability_ddb.py
+++ backend/endpoints/dydb/availability_ddb.py
@@ -22,14 +22,23 @@ router = APIRouter()
 def get_events(event_id: str, user_id: str = Depends(authenticated_uid_check)):
 
     calendar_event = CalendarEvent.get(event_id)
+    booked_slots = []
+    
     
     if calendar_event:
+        for user in calendar_event.event_availability_slots:
+            for timeslot in calendar_event.event_availability_slots[user]:
+                booked_slots.append({
+                    "availability_slot": timeslot,
+                    "availability_owner": user
+               })
+
         return {
             "event_id": event_id,
             "event_title": calendar_event.event_title,
             "event_description": calendar_event.event_description,
             "availableTimeSlots": calendar_event.event_datetime_slots,
-            "booked_slots": [],
+            "booked_slots": booked_slots,
         }
     else:
         raise HTTPException(status_code=404, detail="Event not found")
@@ -41,30 +50,35 @@ def update_availability(
     availabilitySlot: AvailabilitySlotRequest,
     user_id: str = Depends(authenticated_uid_check),
 ):
-    event_obj = {
-        "event_id": availabilitySlot.event_id,
-        "availability_owner": user_id,
-        "availability_slot": availabilitySlot.event_availability_slot,
-    }
 
-    with engine.connect() as connection:
+    calendar_event = CalendarEvent.get(availabilitySlot.event_id)
+
+    if calendar_event:
+
+        event_obj = {
+            "event_id": availabilitySlot.event_id,
+            "availability_owner": user_id,
+            "availability_slot": availabilitySlot.event_availability_slot,
+        }
+
+
         if availabilitySlot.event_action == "TOGGLE":
-            ins = availabilityPDB.insert()
-            connection.execute(ins, event_obj)
+            if user_id not in calendar_event.event_availability_slots:
+                calendar_event.event_availability_slots[user_id] = []
+            calendar_event.event_availability_slots[user_id].append(availabilitySlot.event_availability_slot)
+        
+        
         elif availabilitySlot.event_action == "UNTOGGLE":
-            untoggle_sql_operation = (
-                delete(availabilityPDB)
-                .where(availabilityPDB.c.event_id == availabilitySlot.event_id)
-                .where(availabilityPDB.c.availability_owner == user_id)
-                .where(
-                    availabilityPDB.c.availability_slot
-                    == availabilitySlot.event_availability_slot
-                )
-            )
-            result = connection.execute(untoggle_sql_operation, event_obj)
-            if not result:
-                raise HTTPException(status_code=404, detail="Event not found")
+                if availabilitySlot.event_availability_slot in calendar_event.event_availability_slots[user_id]:
+                    calendar_event.event_availability_slots[user_id].remove(availabilitySlot.event_availability_slot)
+                else:
+                    raise HTTPException(status_code=404, detail="Slot has not been selected.")
+        
 
+    else:
+        raise HTTPException(status_code=404, detail="Event not found")
+
+    calendar_event.save()
     return event_obj
 
 
--- backend/endpoints/models/dynamo_models.py
+++ backend/endpoints/models/dynamo_models.py
@@ -1,6 +1,6 @@
 from pynamodb.models import Model
 from pynamodb.indexes import GlobalSecondaryIndex, AllProjection
-from pynamodb.attributes import UnicodeAttribute, ListAttribute, UnicodeSetAttribute
+from pynamodb.attributes import UnicodeAttribute, ListAttribute, MapAttribute
 # from pynamodb_attributes import UUIDAttribute
 
 class CalendarEvent(Model):
@@ -13,4 +13,4 @@ class CalendarEvent(Model):
     event_title = UnicodeAttribute()
     event_description = UnicodeAttribute()
     event_datetime_slots = ListAttribute(default=list)
-    event_availability_slots = UnicodeSetAttribute(default=dict)
+    event_availability_slots = MapAttribute(default=dict)
